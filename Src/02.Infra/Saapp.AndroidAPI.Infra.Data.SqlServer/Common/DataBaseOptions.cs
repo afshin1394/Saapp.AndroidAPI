be77a4cf99e1abbbeda72ab5c2cd3411ffc92f5f
afshin <72977857+afshin1394@users.noreply.github.com>
2021-12-27 11:09:51 -0800

    Update DataBaseOptions.cs

--- Src/02.Infra/Saapp.AndroidAPI.Infra.Data.SqlServer/Common/DataBaseOptions.cs
+++ Src/02.Infra/Saapp.AndroidAPI.Infra.Data.SqlServer/Common/DataBaseOptions.cs
@@ -9,6 +9,6 @@ namespace Saapp.AndroidAPI.Infra.Data.SqlServer.Common
     public class DatabaseOptions
     {
         public static string SectionName = "Database";
-        public string ConnectionString { get; set; } = "server=192.168.11.3; database=Pakhsh; user id = GrpcsrvTablet; password=Grpc@srv#Tablet; timeout=120";
+        public string ConnectionString { get; set; } = "your connectin string";
     }
 }
